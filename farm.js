b0501077871bdf930c8ba7b7706f6bea8c13b3d8
Doug Beck <doug@douglasbeck.com>
2017-07-19 19:45:03 -0400

    Bird!

--- farm.js
+++ farm.js
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
 'use strict';
 
+const Bird = require('bird-foo');
 const Cow = require('cow-foo');
 const feedCow = require('cow-foo/feed-cow');
 
+const feedBird = require('./lib/feed-bird');
 const eatLunch = require('./lib/eat-lunch');
 
 
 function main(cb) {
   const cow = new Cow();
   feedCow(cow, function() {
-    eatLunch(function() {
-      cb();
-    });
+    var bird = new Bird();
+    if(bird == 'pigeon') {
+      feedBird(bird, 'seed', function() {
+        eatLunch(function() {
+          cb();
+        });
+      });
+    } else {
+      feedBird(bird, 'corn', function() {
+        eatLunch(function() {
+          cb();
+        });
+      });
+    }
   });
 }
 
